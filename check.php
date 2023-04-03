9556581ce3f346a59cc957931dd97d460c642621
wobuhui666 <121330915+wobuhui666@users.noreply.github.com>
2023-04-03 22:35:21 +0800

    Create check.php

---
+++ check.php
@@ -0,0 +1,12 @@
+<?php
+$password = "mypassword"; // 正确的密码
+if ($_POST["password"] == $password) {
+    // 密码正确，跳转到受保护的页面
+    header("Location: protected_page.html");
+    exit();
+} else {
+    // 密码错误，重新输入
+    header("Location: index.html");
+    exit();
+}
+?>
