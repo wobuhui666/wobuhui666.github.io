2541052bea15d7cab50e7f22b0048482c4795ba1
wobuhui666 <121330915+wobuhui666@users.noreply.github.com>
2023-04-03 23:08:11 +0800

    Update check.php

--- check.php
+++ check.php
@@ -2,7 +2,7 @@
 $password = "1"; // 正确的密码
 if ($_POST["password"] == $password) {
     // 密码正确，跳转到受保护的页面
-    header("Location: 6.html");
+    header("Location: https://wobuhui666.github.io/6.html");
     exit();
 } else {
     // 密码错误，重新输入
